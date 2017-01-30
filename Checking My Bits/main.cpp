e64bcbfe2c35b0bdddef6eb4541b1199d10836e7
zomawai.sailo <zomawai.sailo@SEA-8E40F1>
2017-01-30 10:52:56 -0800

    stack templates

--- Checking My Bits/main.cpp
+++ Checking My Bits/main.cpp
@@ -1,27 +1,50 @@
 #include <iostream>
 #include <assert.h>
-#include "hash.h"
+#include "sstack.h"
+#include <string>
 
 void main() {
 
-	hashtable tb;
-	tb.set(1, 10);
-	tb.set(13, 11);
-	tb.set(157, 21);
-	tb.set(17, 441);
+	//hashtable tb;
+	//tb.set(1, 10);
+	//tb.set(13, 11);
+	//tb.set(157, 21);
+	//tb.set(17, 441);
 
-	assert(tb.isSet(1));
-	//assert(tb.count() == 3);
+	//assert(tb.isSet(1));
+	////assert(tb.count() == 3);
 
-	std::cout << "Count: " << tb.count() << std::endl;
+	//std::cout << "Count: " << tb.count() << std::endl;
 
-	std::cout << "tb.get = " << tb.get(157) << std::endl;
+	//std::cout << "tb.get = " << tb.get(157) << std::endl;
 
-	tb.clear();
+	//tb.clear();
+	//
+	//std::cout << "Count: " << tb.count() << std::endl;
+
+	stack<std::string> S;
+
+	S.reserve(50);
+
+	S.push("hello");
+	S.push("food");
+	S.push("dinosaur");
+	S.push("cat memes");
 	
-	std::cout << "Count: " << tb.count() << std::endl;
-
 	
+	std::cout << S.peek() << std::endl;
+
+	S.pop();
+
+	std::cout << S.peek() << std::endl;
+
+	S.pop();
+
+	std::cout << S.peek() << std::endl;
+
+	S.pop();
+
+	std::cout << S.peek() << std::endl;
 
 	system("pause");
 }
--- Checking My Bits/sstack.h
+++ Checking My Bits/sstack.h
@@ -1,23 +1,69 @@
 #pragma once
 #include "hash.h"
 
+template <typename T>
 class stack {
-private: //what data to store, any helper functions
 
-	const static int SIZE = 10;
-	int backing[SIZE];
-	int head = 0;
-	
+private: //what data to store, any helper functions
+	unsigned int defaultSIZE = 10;
+	T *backing;
+	int head;
+	size_t capacity;
 
 public:
-	void push(int value);
+	stack() {
+		backing = new T[defaultSIZE];
+		head = 0;
+		capacity = defaultSIZE;
+	};
+	~stack() {
+		delete[] backing;
+	};
+	void push(T value) {
+		if (head > capacity)
+			reserve(head * 1.5f);
+		backing[head] = value;
+		head++;
+	};
 
-	int pop();
+	T pop() {
+		head--;
+		return backing[head];
+	};
 
-	int peek();
+	T peek() {
+		return backing[head - 1];
+	};
 
-	size_t size();
+	size_t size() {
+		return head;
+	};
 
-	bool empty();
-	void clear();
+	void reserve(size_t size) {
+		T * tempArray = new T[size];
+
+		//copy by element
+		for (int i = 0; i < head; ++i)
+			tempArray[i] = backing[i];
+
+		//copy memory block
+		memcpy_s(tempArray, sizeof(int) * size, backing, sizeof(int) * head);
+	};
+
+	void compact() {
+		if (capacity > head * 1.5f) {
+			capacity = head * 1.5f;
+		}
+		return;
+	};
+	size_t volume() {
+		return capacity;
+	};
+
+	bool empty() {
+		return head > 0;
+	};
+	void clear() {
+		head = 0;
+	};
 };
